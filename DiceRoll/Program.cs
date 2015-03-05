e6af065ddb86ad334b7d49e0aabe7e1674400f5b
DavidWright925 <GitHub@WrightHere.com>
2015-03-05 14:38:05 -0800

    Added main DiceRoll code.

--- DiceRoll/Program.cs
+++ DiceRoll/Program.cs
@@ -8,8 +8,24 @@ namespace DiceRoll
 {
     class Program
     {
+        //http://cs.dvc.edu/intro2programming/index.jsp?java#step1
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int a; // the name of the first die
+            int b; // the name of the second die
+            int total; // the result
+            a = random.Next(1, 6);
+            b = random.Next(1, 6);
+            total = a + b;
+
+            Console.Write("Dice result: ");
+            Console.Write(total);
+            Console.Write(" (");
+            Console.Write(a);
+            Console.Write(" and ");
+            Console.Write(b);
+            Console.WriteLine(")");
         }
     }
 }
